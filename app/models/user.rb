2a63cd9df493b87299f8b8f5aabd7316467772bc
muromoto-y <6610010@gmail.com>
2020-08-14 10:57:35 +0900

    新規登録、ログイン遷移

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+   validates :first_name, :last_name, :first_name_furigana, :last_name_furigana, :birthday, :nick_name, :email, :password, presence: true
 end
