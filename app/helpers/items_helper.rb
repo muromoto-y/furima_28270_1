524b69f0a797ff48af20990c950aa3e121e26ae5
muromoto-y <6610010@gmail.com>
2020-08-15 15:35:36 +0900

    dbに保存できた

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,2 @@
-module ItemHelper
+module ItemsHelper
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-   validates :first_name, :last_name, :first_name_furigana, :last_name_furigana, :birthday, :nick_name, :email, :password, presence: true
+
+  has_many :items
+  has_many :purchases
+         
+  validates :first_name, :last_name, :first_name_furigana, :last_name_furigana, :birthday, :nickname, presence: true
+  validates :password, presence: true, length: {minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i}
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
 end
