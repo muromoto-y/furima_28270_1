e52d91d1575c69635f29a01c6209e5bd150f896c
muromoto-y <6610010@gmail.com>
2020-08-19 01:23:43 +0900

    カテゴリー設置

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
   belongs_to :user
   has_one :purchase
   has_one_attached :image
 
   with_options presence: true do
-    validates
-    validates
+    # validates
+    # validates
   end
 end
