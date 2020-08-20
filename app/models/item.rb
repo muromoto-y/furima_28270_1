ca21af1ae475da6d0b6bc191dcdb472aebfabc89
muromoto-y <6610010@gmail.com>
2020-08-20 14:32:50 +0900

    出品はログインしていないとできない

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,16 +6,17 @@ class Item < ApplicationRecord
   has_one :purchase
   has_one_attached :image
 
-  with_options presence: true do
-    validates :name,            
-    validates :explanation,     
-    validates :price,           numericality: { only_interger: true }
-                                              { greater_than: 299 }
-                                              { less_than: 10000000 }
-    validates :category,        numericality: { greater_than: 1 }
-    validates :status,          numericality: { greater_than: 1 }
-    validates :shipping_burden, numericality: { greater_than: 1 }
-    validates :shipping_area,   numericality: { greater_than: 1 }
-    validates :shipping_days,   numericality: { greater_than: 1 }
-  end
+  # with_options presence: true do
+  #   validates :name,            
+  #   validates :explanation,     
+  #   validates :price,           numericality: { only_interger: true },
+  #                                             { greater_than: 299 },
+  #                                             { less_than: 10000000 }
+  #   validates :category,        numericality: { greater_than: 1 }
+  #   validates :status,          numericality: { greater_than: 1 }
+  #   validates :shipping_burden, numericality: { greater_than: 1 }
+  #   validates :shipping_area,   numericality: { greater_than: 1 }
+  #   validates :shipping_days,   numericality: { greater_than: 1 }
+
+  # end
 end
