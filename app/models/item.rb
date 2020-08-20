1b1df47d5fefe661f76ecb3cf8b7cbbe57219b4e
muromoto-y <6610010@gmail.com>
2020-08-20 22:06:37 +0900

    エラー文を日本語に変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,13 +11,13 @@ class Item < ApplicationRecord
     validates :explanation         
     validates :image               
     validates :price,              numericality: { only_interger: true,
-                                                   greater_than: 299,
-                                                   less_than: 10000000 }
-    validates :category_id,        numericality: { greater_than: 1 }
-    validates :status_id,          numericality: { greater_than: 1 }
-    validates :shipping_burden_id, numericality: { greater_than: 1 }
-    validates :shipping_area_id,   numericality: { greater_than: 1 }
-    validates :shipping_days_id,   numericality: { greater_than: 1 }
+                                                   greater_than: 299 ,
+                                                   less_than: 10000000 , message: "価格は300円~9,999,999円で設定してください" }
+    validates :category_id,        numericality: { greater_than: 1 , message: "カテゴリーを選択してください"}
+    validates :status_id,          numericality: { greater_than: 1 , message: "状態を選択してください"}
+    validates :shipping_burden_id, numericality: { greater_than: 1 , message: "送料の負担者を選択してください"}
+    validates :shipping_area_id,   numericality: { greater_than: 1 , message: "配送元の地域を選択してください"}
+    validates :shipping_days_id,   numericality: { greater_than: 1 , message: "発送までの期間を選択してください"}
 
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,28 +34,27 @@ describe Item, type: :model do
       it "カテゴリーidが1だと出品できない" do
         @item.category_id = 1
         @item.valid?
-        binding.pry
-        expect(@item.errors.full_messages).to include("Category must be greater than 1")
+        expect(@item.errors.full_messages).to include("Category カテゴリーを選択してください")
       end
       it "商品の状態の情報のidが1だと出品できない" do
         @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status must be greater than 1")
+        expect(@item.errors.full_messages).to include("Status 状態を選択してください")
       end
       it "配送料の負担の情報のidが1だと出品できない" do
         @item.shipping_burden_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping burden must be greater than 1")
+        expect(@item.errors.full_messages).to include("Shipping burden 送料の負担者を選択してください")
       end
       it "発送元の地域の情報のidが1だと出品できない" do
         @item.shipping_area_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping area must be greater than 1")
+        expect(@item.errors.full_messages).to include("Shipping area 配送元の地域を選択してください")
       end
       it "発送までの日数の情報のidが1だと出品できない" do
         @item.shipping_days_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping days must be greater than 1")
+        expect(@item.errors.full_messages).to include("Shipping days 発送までの期間を選択してください")
       end
       it "価格の情報がないと出品できない" do
         @item.price = ""
@@ -65,12 +64,12 @@ describe Item, type: :model do
       it "価格が299円以下なら出品できない" do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than 299")
+        expect(@item.errors.full_messages).to include("Price 価格は300円~9,999,999円で設定してください")
       end
       it "価格が10000000円以上なら出品できない" do
         @item.price = 10000000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+        expect(@item.errors.full_messages).to include("Price 価格は300円~9,999,999円で設定してください")
       end
       it "画像が０枚だと出品できない" do
         @item.image = nil
