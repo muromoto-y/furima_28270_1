3101679b805a5f4e9948ef4d68fda5c4cb97511f
muromoto-y <6610010@gmail.com>
2020-08-20 13:27:24 +0900

    テストコード準備

---
+++ spec/factories/item.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :item do
+    name            {"せン濯ki"}
+    explanation     {"aaaアアアあああ亜唖々000"}
+    price           {"300"}
+    category        {"2"}
+    status          {"2"}
+    shipping_burden {"2"}
+    shipping_area   {"2"}
+    shipping_days   {"2"}
+  end
+end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+describe Item, type: :model do
+
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品がうまくいくとき' do
+      it
+    end
+  end
+end  
