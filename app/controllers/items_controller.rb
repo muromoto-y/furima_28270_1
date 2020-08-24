f97334ba3c420f237a5a489aa7f41952083f3980
muromoto-y <6610010@gmail.com>
2020-08-24 21:47:16 +0900

    shuusei

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action　@item = Item.find(params[:id])
+  before_action :set_item, onry: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -37,6 +37,11 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 
   private
 
